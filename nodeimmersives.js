305931fc0ede5ec11ea0b0434f29789da6a47453
cesar juarez <cesar.juarez@codeimmersives.com>
2021-02-24 07:05:23 -0800

    my code

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,2 +1,9 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
+const firstName = getInput (0); 
+const lastName = getInput (1);
+const fullName = firstName + " " + lastName;
+console.log ('hello. ' + " " + firstName + " " + lastName);
+console.log ("your name in capital letters is: "  + fullName.toUpperCase ());
+console.log ( "your Initials are: "  + fullName[0] + '.' + fullName[fullName.indexOf(' ') + 1] + '.');
+console.log (firstName + lastName + "@codeimmersives.com");
